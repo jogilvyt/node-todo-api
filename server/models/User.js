241c5975d5a0118b66a7ef03ec8fc603c859070c
Jack Taylor <Jack.Taylor@emea.adecco.net>
2018-07-16 10:13:22 +0100

    Add POST /users/login route

--- server/models/User.js
+++ server/models/User.js
@@ -75,6 +75,30 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  const User = this;
+
+  return User.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) {
+          return reject(err);
+        }
+
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      })
+    });
+  });
+};
+
 UserSchema.pre('save', function(next) {
   const user = this;
 
